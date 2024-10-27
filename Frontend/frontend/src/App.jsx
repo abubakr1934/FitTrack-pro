43c858c8d77ca71697cada86e93f0d82adefa102
Vaibhav Pimpalkar <vaibhavpimpalkar45@gmail.com>
2024-10-27 19:51:42 +0530

    commit

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -3,13 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+import Login from './landingPage/authenticationPages/Login'
+import Signup from './landingPage/authenticationPages/Signup'
+import Features from './pageComponents/features/Features'
+import Collection from './pageComponents/Collection'
+import Footer from './pageComponents/Footer'
+import HeroSection from './pageComponents/HeroSection'
+import Navbar from './pageComponents/Navbar'
+import Reviews from './pageComponents/Reviews'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+    <div>
+      <Navbar />
+      <HeroSection />
+      <Features />
+      <Collection />
+      <Reviews />
+      <Footer />
+    </div>
   )
 }
 
